ed9701534f37f1a1e2e038154b77bb3f71d668a7
智库 <wangsn.tech@outlook.com>
2024-09-22 15:16:29 +0800

    feat: 新增测试接口-hello，并使用http文件测试接口

--- business/src/main/java/com/zhiku/nls/business/BusinessApplication.java
+++ business/src/main/java/com/zhiku/nls/business/BusinessApplication.java
@@ -1,13 +1,21 @@
 package com.zhiku.nls.business;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+@Slf4j
 @SpringBootApplication
 public class BusinessApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BusinessApplication.class, args);
+
+        ConfigurableEnvironment environment = SpringApplication.run(BusinessApplication.class, args).getEnvironment();
+
+        log.info("----------启动成功！----------");
+        log.info("访问地址: \thttp://localhost:{}{}", environment.getProperty("server.port"), environment.getProperty("server.servlet.context-path"));
+        log.info("----------------------------");
     }
 
 }
